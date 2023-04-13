251c5f2c16b0e472d038f274546a4d5598580426
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-04-13 19:25:50 +0200

    version stable, projet pratiquement fini

--- FrontEnd/assets/javascript/index.js
+++ FrontEnd/assets/javascript/index.js
@@ -3,9 +3,46 @@ const objets = document.querySelector(".objets");
 const apparts = document.querySelector(".appart");
 const hotel = document.querySelector(".hotel");
 const tous = document.querySelector(".tous");
-token = localStorage.token
-console.log(token)
+token = localStorage.token;
+let select = document.getElementById("input_category");
+
 //Affiche les images
+window.addEventListener("load", () => {
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((categorie) => {
+        let option = document.createElement("option");
+        option.value = categorie.id;
+        option.text = categorie.name;
+        select.add(option);
+      });
+    });
+});
+
+function deleteProject(id) {
+  fetch("http://localhost:5678/api/works/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const figure_gallery = document.querySelector("#work-" + id);
+      console.log(figure_gallery);
+      if (figure_gallery) {
+        figure_gallery.remove();
+      }
+    })
+    .catch((err) => console.log("Il y a un problème : " + err));
+}
 
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
@@ -81,7 +118,10 @@ fetch("http://localhost:5678/api/works")
     afficherImage(works); // display all images at the beginning
   })
   .catch((error) => console.error(error));
+
 const authentified = localStorage.token;
+console.log(authentified);
+
 //gerer la page lorsque connecté
 filtres = document.querySelector("#filtres");
 if (authentified !== undefined) {
@@ -124,7 +164,6 @@ if (authentified !== undefined) {
           trash.src = "./assets/icons/trash.svg";
           trash.classList.add("modale_image_trash");
           trash.addEventListener("click", () => {
-            console.log(works[i].id);
             deleteProject(works[i].id);
             // Remove the work from the DOM
             modale_images.removeChild(figure);
@@ -159,73 +198,77 @@ if (authentified !== undefined) {
           // envoi du formulaire
           image_input = document.querySelector("#real_image_form");
           form_container = document.querySelector(".modale_container_form");
-          const photo = document.getElementById("ajout_photo");
-          const category = document.getElementById("input_category");
-          const title = document.getElementById("input_title");
-          const maxSize = 4000000;
+          let photo = document.getElementById("ajout_photo");
+          let category = document.getElementById("input_category");
+          let title = document.getElementById("input_title");
+          let valider = document.querySelector(".valider");
+          let image;
+          let titre;
+          const formData = new FormData();
+
+          let maxSize = 4000000;
           let category_inputed = 0;
-          image_input.addEventListener("change", () => {
-            fetch("http://localhost:5678/api/categories")
-              .then((response) => response.json())
-              .then((data) => {
-                const categories = data;
-                console.log(categories);
-                // Parcours de la liste des catégories pour récupérer l'id correspondant à la catégorie sélectionnée
-                for (let i = 0; i < categories.length; i++) {
-                  if (category.value === categories[i].name) {
-                    category_inputed = categories[i].id;
-                  }
-                }
-                // Récupération de l'image et du titre
-                const image = document.getElementById("real_image_form").files[0];
-                const titre = document.getElementById("input_title").value;
-                // Vérification de la taille de l'image
 
-                if (image.size < maxSize) {
-                  // Création du formulaire pour l'envoi des données
-                  const formData = new FormData();
-                  formData.append("image", image);
-                  formData.append("title", titre);
-                  formData.append("category", category_inputed);
+          //montrer la preview de l'image
+          function showPreview(e) {
+            const src = URL.createObjectURL(e.target.files[0]);
+            const preview = document.createElement("img");
+            preview.classList.add("preview")
+            console.log(src)
+            preview.src = src;
+            form_container.innerHTML = "";
 
-                  // Envoi des données à l'API via une requête POST
+            form_container.appendChild(preview);
+            console.log(files);
+          }
 
-                  fetch("http://localhost:5678/api/works", {
-                    method: "POST",
-                    headers: {
-                      Authorization: `Bearer ${token}`,
-                      accept: "application/json",
-                    },
-                    body: formData,
-                  }).catch((error) => console.error(error));
-                } else {
-                  // Affichage d'un message d'erreur si la taille de l'image est trop grande
-                  const erreur_taille = document.querySelector("#erreur_taille");
-                  //erreur_taille.classList.remove("remove");
-                }
-              });
+          real_form_btn.addEventListener("change", (e) => {
+            showPreview(e);
+            
+          });
+
+          function createForm() {
+            if (image.size < maxSize) {
+              // Création du formulaire pour l'envoi des données
+              formData.append("image", image);
+              formData.append("title", titre);
+              formData.append("category", option);
+            } else {
+              // Affichage d'un message d'erreur si la taille de l'image est trop grande
+              const erreur_taille = document.querySelector("#erreur_taille");
+              //erreur_taille.classList.remove("remove");
+            }
+            // Envoi des données à l'API via une requête POST
+            fetch("http://localhost:5678/api/works", {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                accept: "application/json",
+              },
+              body: formData,
+            }).catch((error) => console.error(error));
+          }
+
+          valider.addEventListener("click", () => {
+            function envoiRequete() {
+              titre = document.getElementById("input_title").value;
+              image = document.getElementById("real_image_form").files[0];
+              option = document.getElementById("input_category").value;
+             
+              console.log(titre)
+              console.log(image)
+              console.log(option)
+
+              if(titre !== undefined && image !== undefined && option !== undefined){
+                createForm();
+
+              } else {
+                console.log("error")
+              }
+            }
+            envoiRequete();
           });
         });
-      })
-      .catch((error) => console.error(error));
+      });
   });
-
-  function deleteProject(id) {
-    fetch("http://localhost:5678/api/works/" + id, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        const figure_gallery = document.querySelector("#work-" + id);
-        console.log(figure_gallery);
-        if (figure_gallery) {
-          figure_gallery.remove();
-        }
-      })
-      .catch((err) => console.log("Il y a un problème : " + err));
-  }
 }
